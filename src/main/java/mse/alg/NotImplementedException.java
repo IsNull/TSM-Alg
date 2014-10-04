04270224e9dfd6c9c5ff97e289153530195ed274
IsNull <isnull@securityvision.ch>
2014-10-04 11:21:24 +0200

    initial commit of horizon find alg

---
+++ src/main/java/mse/alg/NotImplementedException.java
@@ -0,0 +1,8 @@
+package mse.alg;
+
+/**
+ * Thrown when a method is access which is not yet implemented.
+ * Created by IsNull on 04.10.14.
+ */
+public class NotImplementedException extends RuntimeException {
+}
