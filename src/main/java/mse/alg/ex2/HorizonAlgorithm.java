e5544d4d7f5f2ceefa8d6614e9844f8f49fc956e
IsNull <isnull@securityvision.ch>
2014-10-04 12:04:08 +0200

    horizon divide & conquer
    Merge is still missing and will be resolved with a plane sweep

--- src/main/java/mse/alg/ex2/HorizonAlgorithm.java
+++ src/main/java/mse/alg/ex2/HorizonAlgorithm.java
@@ -1,7 +1,5 @@
 package mse.alg.ex2;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.List;
 
 /**
@@ -9,7 +7,35 @@ import java.util.List;
  */
 public class HorizonAlgorithm {
 
+    /**
+     * Compute the horizon from the x sorted chains.
+     *
+     * @param chains
+     * @return
+     */
     public Horizon computeHorizon(List<MonotoneChain> chains){
-        throw new NotImplementedException();
+        return computeHorizon(chains, 0, chains.size()-1);
     }
+
+    /**
+     * Recursive horizon computation which shares the chains list instance.
+     * @param chains
+     * @param start Start pointer for the current chain list
+     * @param end End pointer for the current chain list
+     * @return
+     */
+    private Horizon computeHorizon(List<MonotoneChain> chains, int start, int end){
+        if(start == end){
+            // Simple case - Conquer it!
+            return new Horizon(chains.get(start));
+        }else{
+            // Too complex for us - Further divide!
+            Horizon part1 = computeHorizon(chains, start, end / 2);
+            Horizon part2 = computeHorizon(chains, end / 2 + 1, end);
+
+            return part1.merge(part2);
+        }
+    }
+
+
 }
