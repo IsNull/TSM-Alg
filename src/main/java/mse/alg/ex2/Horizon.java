044ce146d3bf99679d9b66192b1c4dcb28addf36
IsNull <isnull@securityvision.ch>
2014-10-08 20:16:05 +0200

    refactored merge method

--- src/main/java/mse/alg/ex2/Horizon.java
+++ src/main/java/mse/alg/ex2/Horizon.java
@@ -1,7 +1,7 @@
 package  mse.alg.ex2;
 
-import com.vividsolutions.jts.geom.LineString;
 import java.util.LinkedList;
+
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.GeometryFactory;
 import com.vividsolutions.jts.geom.LineString;
@@ -10,13 +10,15 @@ import com.vividsolutions.jts.geom.MultiLineString;
 /**
  * x-monotone linked list of strictly x-monotone chains
  * @author Christoph Stamm
- *
+ * @author E. Neher
+ * @author S. Marcin
+ * @author P. Buettiker
  */
 public class Horizon {
 	private LinkedList<MonotoneChain> m_chains = new LinkedList<>();	// x-monotone linked list of chains
-	
+
 	private Horizon() {	}
-	
+
 	/**
 	 * Creates new horizon of one monotone chain
 	 * 
@@ -25,15 +27,15 @@ public class Horizon {
 	public Horizon(MonotoneChain mc) {
 		m_chains.add(mc);
 	}
-	
+
 	public boolean isEmpty() {
 		return m_chains.size() == 0;
 	}
-	
+
 	public int size() {
 		return m_chains.size();
 	}
-	
+
 	/**
 	 * Merges two horizons (this and h) in O(n + k) time and returns the merged horizon.
 	 * Uses a plane sweep approach.
@@ -41,83 +43,47 @@ public class Horizon {
 	 * @param h second horizon
 	 * @return merged horizon
 	 */
-    public Horizon merge(Horizon h) {
-        Horizon horizon = new Horizon();
-        SweepLine<Status> sl = new SweepLine<Status>(new Status(horizon));
+	public Horizon merge(Horizon h) {
+		Horizon horizon = new Horizon();
+		SweepLine<Status> sl = new SweepLine<Status>(new Status(horizon));
 
-        /*
-        //Debug
-        System.out.println("--- Start ---");
-        for(int j=0; j < this.m_chains.size(); j++) {
-            for (int i = 0; i < this.m_chains.get(j).size(); i++) {
-                System.out.println(j + "-this: " + this.m_chains.get(j).get(i).toString());
-            }
-        }
-        for(int j=0; j < h.m_chains.size(); j++) {
-            for (int i = 0; i < h.m_chains.get(j).size(); i++) {
-                System.out.println(j + "-h: " + h.m_chains.get(j).get(i).toString());
-            }
-        }
-        */
+		// printDebug(h);
 
-        SortHorizon sort1 = new SortHorizon(this.m_chains);
-        SortHorizon sort2 = new SortHorizon(h.m_chains);
+		SortHorizon sort1 = new SortHorizon(this.m_chains);
+		SortHorizon sort2 = new SortHorizon(h.m_chains);
 
-        while(!sort1.isFinished() || !sort2.isFinished()) {
-            if (!sort1.isFinished()) {
-                if (sort2.isFinished() || sort1.getX() <= sort2.getX()) {
+		while(!sort1.isFinished() || !sort2.isFinished()) {
+			if (!sort1.isFinished()) {
+				if (sort2.isFinished() || sort1.getX() <= sort2.getX()) {
+					emitEvent(sl, sort1);
+					continue;
+				}
+			}
+			if (!sort2.isFinished()) {
+				if (sort1.isFinished() || sort2.getX() <= sort1.getX()) {
+					emitEvent(sl, sort1);
+					continue;
+				}
+			}
+		}
 
-                    if (sort1.isNextStart()) {
-                        //System.out.println("Start-S1-Index:"+sort1.getX());
-                        sl.addEvent(new StartEvent(sl, sort1.getX(), sort1.getNext()));
-                        continue;
-                    }
-                    if (sort1.isNextStop()) {
-                        //System.out.print("Stop-S1-Index:"+sort1.getX());
-                        sl.addEvent(new StopEvent(sl, sort1.getX(), sort1.getNext(), sort1.getStopIndex()));
-                        //System.out.println("-->"+sort1.getStopIndex());
-                        continue;
-                    }
-                    if (sort1.isNextInner()) {
-                        //System.out.print("Inner-S1-Index:"+sort1.getX());
-                        sl.addEvent(new InnerEvent(sl, sort1.getX(), sort1.getNext(), sort1.getIndex() - 1));
-                        //System.out.println("-->"+(sort1.getIndex() - 1));
-                        continue;
-                    }
+		sl.process();
+		assert horizon.isValid() : "invalid horizon";
+		return horizon;
+	}
 
-                }
-            }
-            if (!sort2.isFinished()) {
-                if (sort1.isFinished() || sort2.getX() <= sort1.getX()) {
+	private void emitEvent(SweepLine<Status> sl, SortHorizon sortHorizon){
+		if (sortHorizon.isNextStart()) {
+			sl.addEvent(new StartEvent(sl, sortHorizon.getX(), sortHorizon.getNext()));
+		}else if (sortHorizon.isNextStop()) {
+			sl.addEvent(new StopEvent(sl, sortHorizon.getX(), sortHorizon.getNext(), sortHorizon.getStopIndex()));
+		}else if (sortHorizon.isNextInner()) {
+			sl.addEvent(new InnerEvent(sl, sortHorizon.getX(), sortHorizon.getNext(), sortHorizon.getIndex() - 1));
+		}else{
+			throw new IllegalStateException("Something went wrong!");
+		}
+	}
 
-                    if (sort2.isNextStart()) {
-                        //System.out.println("Start-S2-Index:"+sort2.getX());
-                        sl.addEvent(new StartEvent(sl, sort2.getX(), sort2.getNext()));
-                        continue;
-                    }
-                    if (sort2.isNextStop()) {
-                        //System.out.print("Stop-S2-Index:"+sort2.getX());
-                        sl.addEvent(new StopEvent(sl, sort2.getX(), sort2.getNext(), sort2.getStopIndex()));
-                        //System.out.println("-->"+sort2.getStopIndex());
-                        continue;
-                    }
-                    if (sort2.isNextInner()) {
-                        //System.out.print("Inner-S2-Index:"+sort2.getX());
-                        sl.addEvent(new InnerEvent(sl, sort2.getX(), sort2.getNext(), sort2.getIndex() - 1));
-                        //System.out.println("-->"+(sort2.getIndex() - 1));
-                        continue;
-                    }
-                }
-
-            }
-
-        }
-
-        sl.process();
-        assert horizon.isValid() : "invalid horizon";
-        return horizon;
-    }
-	
 	/**
 	 * Checks x-monotonicity of this horizon in O(n) time
 	 * @return true if x-monotone
@@ -131,7 +97,7 @@ public class Horizon {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Interface to JTS
 	 * @return LineString without duplicated points
@@ -140,14 +106,14 @@ public class Horizon {
 		// count coordinates
 		int cnt = 0;
 		MonotoneChain prev = null;
-		
+
 		for(MonotoneChain mc: m_chains) {
 			if (prev != null && prev.isLeftSiblingOf(mc))
 				cnt--;
 			cnt += mc.size();
 			prev = mc;
 		}
-		
+
 		// collect coordinates
 		Coordinate[] coords = new Coordinate[cnt];
 		cnt = 0;
@@ -162,10 +128,10 @@ public class Horizon {
 			}
 			prev = mc;
 		}
-		
+
 		return new GeometryFactory().createLineString(coords);
 	}
-	
+
 	/**
 	 * Interface to JTS
 	 * @return MultiLineString of all chains in this horizon
@@ -173,13 +139,13 @@ public class Horizon {
 	public MultiLineString toMultiLineString() {
 		LineString[] chains = new LineString[m_chains.size()];
 		int i = 0;
-		
+
 		for(MonotoneChain mc: m_chains) {
 			chains[i++] = mc.toLineString();
 		}
 		return new GeometryFactory().createMultiLineString(chains);
 	}
-	
+
 	/**
 	 * Add new chain to horizon
 	 * @param mc x-monotone chain
@@ -190,5 +156,19 @@ public class Horizon {
 			m_chains.add(mc);
 		}
 	}
-	
+
+	private void printDebug(Horizon h){
+		System.out.println("--- Start ---");
+		for(int j=0; j < this.m_chains.size(); j++) {
+			for (int i = 0; i < this.m_chains.get(j).size(); i++) {
+				System.out.println(j + "-this: " + this.m_chains.get(j).get(i).toString());
+			}
+		}
+		for(int j=0; j < h.m_chains.size(); j++) {
+			for (int i = 0; i < h.m_chains.get(j).size(); i++) {
+				System.out.println(j + "-h: " + h.m_chains.get(j).get(i).toString());
+			}
+		}
+	}
+
 }
